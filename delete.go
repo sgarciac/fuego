fb1312cabf83b78c662f916e2c868ca05c2fd3f3
sergio garcia <sergio.garcia@gmail.com>
2020-10-03 21:10:59 -0400

    Adds support for using document paths

--- delete.go
+++ delete.go
@@ -1,21 +1,36 @@
 package main
 
 import (
+	firestore "cloud.google.com/go/firestore"
 	"context"
 	"fmt"
 	"github.com/urfave/cli"
 )
 
 func deleteCommandAction(c *cli.Context) error {
-	collectionPath := c.Args().First()
-	id := c.Args().Get(1)
+	argsLength := len(c.Args())
+
+	if argsLength < 1 || argsLength > 2 {
+		return cli.NewExitError("Wrong number of arguments", 82)
+	}
+
 	client, err := createClient(credentials)
 	if err != nil {
 		return cliClientError(err)
 	}
 
-	collectionRef := client.Collection(collectionPath)
-	documentRef := collectionRef.Doc(id)
+	var documentRef *firestore.DocumentRef
+
+	if argsLength == 2 {
+		collectionPath := c.Args().First()
+		id := c.Args().Get(1)
+		collectionRef := client.Collection(collectionPath)
+		documentRef = collectionRef.Doc(id)
+	} else {
+		documentPath := c.Args().First()
+		documentRef = client.Doc(documentPath)
+	}
+
 	res, err := documentRef.Delete(context.Background())
 	if err != nil {
 		return cli.NewExitError(fmt.Sprintf("Failed to delete data. \n%v", res), 82)
