9407643fc416447b005ac471e1b60c218f6a0109
雷天超 <leitianchao123@163.com>
2021-04-19 11:09:37 +0800

    refactor(client): eliminate the duplicated code in client creation

--- client.go
+++ client.go
@@ -14,31 +14,7 @@ import (
 // default projectid if none has been set.
 
 func createClient(credentials string) (*firestore.Client, error) {
-	var err error
-	var firebaseApp *firebase.App
-	if os.Getenv("FIRESTORE_EMULATOR_HOST") != "" {
-		if projectId == "" {
-			projectId = "default"
-		}
-		client, err := firestore.NewClient(context.Background(), projectId)
-		return client, err
-	} else if credentials != "" {
-		sa := option.WithCredentialsFile(credentials)
-		config := getConfig()
-		firebaseApp, err = firebase.NewApp(context.Background(), config, sa)
-		if err != nil {
-			return nil, err
-		}
-		return firebaseApp.Firestore(context.Background())
-	} else {
-		// This will use GOOGLE_APPLICATION_CREDENTIALS
-		config := getConfig()
-		firebaseApp, err = firebase.NewApp(context.Background(), config)
-		if err != nil {
-			return nil, err
-		}
-		return firebaseApp.Firestore(context.Background())
-	}
+	return createClientWithProjectId(credentials, projectId)
 }
 
 func createClientWithProjectId(credentials string, projectId string) (*firestore.Client, error) {
@@ -67,14 +43,6 @@ func createClientWithProjectId(credentials string, projectId string) (*firestore
 		}
 		return firebaseApp.Firestore(context.Background())
 	}
-}
-
-func getConfig() *firebase.Config {
-	config := firebase.Config{}
-	if projectId != "" {
-		config.ProjectID = projectId
-	}
-	return &config
 }
 
 func getConfigWithProjectId(projectId string) *firebase.Config {
