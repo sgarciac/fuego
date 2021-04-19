309fa184e75b5df64dc5cb89036aa7eecec1427c
雷天超 <leitianchao123@163.com>
2021-04-19 11:03:10 +0800

    refactor(copy): limit the number of goroutines when copying

--- copy.go
+++ copy.go
@@ -8,7 +8,8 @@ import (
 	"google.golang.org/api/iterator"
 	"log"
 	"strings"
-	"sync"
+	"sync/atomic"
+	"time"
 )
 
 type PathType int
@@ -116,84 +117,274 @@ func copyCommandAction(c *cli.Context) error {
 }
 
 func copyDocument(source, target *firestore.DocumentRef, option CopyOption)  {
-	targetDoc, err := target.Get(context.Background())
+	client := NewCopyClient(200, option)
 
-	if targetDoc == nil {
-		if err != nil {
-			log.Fatalf("get document %s error: %s\n", target.Path, err)
-			return
-		}
-		return
-	}
-
-	if targetDoc.Exists() && !option.overwrite {
-		log.Fatalf("skipped document %s, because it already exists. use --override to override \n", target.Path)
-		return
-	}
-
-	sourceDoc, err := source.Get(context.Background())
-
-	if sourceDoc == nil {
-		if err != nil {
-			log.Fatalf("get document %s error: %s\n", source.Path, err)
-			return
-		}
-		return
-	}
-
-	var options []firestore.SetOption
-	if option.merge {
-		options = append(options, firestore.MergeAll)
-	}
-
-	if sourceDoc.Exists() {
-		_, err := target.Set(context.Background(), sourceDoc.Data(), options...)
-		if err != nil {
-			log.Fatalf("copy document %s to %s error: %s\n", source.Path, target.Path, err)
-			return
-		}
-	}
-
-	var wg sync.WaitGroup
-	defer wg.Wait()
-
-	collections := source.Collections(context.Background())
-
-	for {
-		next, err := collections.Next()
-		if err == iterator.Done {
-			return
-		}
-		if err != nil {
-			log.Fatal(err)
-			return
-		}
-
-		wg.Add(1)
-		go func(s, t *firestore.CollectionRef, wwg *sync.WaitGroup) {
-			defer wwg.Done()
-			copyCollection(s, t, option)
-		}(next, target.Collection(next.ID), &wg)
-	}
+	client.Run(
+		NewDocumentCopyJob(source, target),
+		NewCollectionIterationJob(source, target),
+		)
 }
 
 func copyCollection(source, target *firestore.CollectionRef, option CopyOption)  {
-	refs := source.DocumentRefs(context.Background())
-	var wg sync.WaitGroup
-	defer wg.Wait()
-	for {
-		next, err := refs.Next()
+	client := NewCopyClient(200, option)
+
+	client.Run(NewDocumentIterationJob(source, target))
+}
+
+type CopyClient struct {
+	jobQueue []CopyJob
+	workerQueue   []chan CopyJob
+	jobChannel    chan CopyJob
+	workerChannel chan chan CopyJob
+	WorkerCount int
+	Option CopyOption
+}
+
+func NewCopyClient(workerCount int, option CopyOption) CopyClient {
+	return CopyClient{
+		jobChannel:    make(chan CopyJob),
+		workerChannel: make(chan chan CopyJob),
+		WorkerCount:   workerCount,
+		Option:        option,
+	}
+}
+
+func (client *CopyClient) Run(seeds... CopyJob)  {
+	out := make(chan []CopyJob)
+
+	go func() {
+		// submit initial jobs
+		for _, j := range seeds {
+			client.submitJob(j)
+		}
+
+		for {
+			// read jobs from workers
+			jobs := <-out
+			for _, j := range jobs {
+				client.submitJob(j)
+			}
+		}
+	}()
+
+	// create works to handle jobs
+	client.createWorkers(out)
+
+	// match workers with jobs
+	done := client.scheduleJobs()
+
+	<- done
+}
+
+func (client *CopyClient) submitJob(job CopyJob)  {
+	client.jobChannel <- job
+}
+
+func (client *CopyClient) workerReady(w chan CopyJob)  {
+	client.workerChannel <- w
+}
+
+func (client *CopyClient) workerInput() chan CopyJob  {
+	return make(chan CopyJob)
+}
+
+func (client *CopyClient) createWorkers(workerOutput chan []CopyJob) {
+	for i:=0; i < client.WorkerCount; i++ {
+		in := client.workerInput()
+		go func(in chan CopyJob) {
+			for {
+				client.workerReady(in)
+				j := <-in
+				w := CopyWorker{
+					Overwrite: client.Option.overwrite,
+					Merge:     client.Option.merge,
+				}
+				jobs, err := w.handleJob(j)
+				if err != nil {
+					continue
+				}
+				workerOutput <- jobs
+			}
+		}(in)
+	}
+}
+
+func (client *CopyClient) scheduleJobs() <- chan struct{} {
+	done := make(chan struct{})
+	go func() {
+		j := <- client.jobChannel
+		client.jobQueue = append(client.jobQueue, j)
+
+		for {
+			var activeJob CopyJob
+			var activeWorker chan CopyJob
+
+			if len(client.jobQueue) > 0 && len(client.workerQueue) > 0 {
+				activeJob = client.jobQueue[0]
+				activeWorker = client.workerQueue[0]
+			}
+
+			select {
+			case jj := <- client.jobChannel:
+				client.jobQueue = append(client.jobQueue, jj)
+			case w := <- client.workerChannel:
+				client.workerQueue = append(client.workerQueue, w)
+			case activeWorker <- activeJob:
+				client.jobQueue = client.jobQueue[1:]
+				client.workerQueue = client.workerQueue[1:]
+			case <- time.After(time.Second * 2):
+				// no more jobs
+				if len(client.jobQueue) == 0 && len(client.workerQueue) == client.WorkerCount {
+					done <- struct{}{}
+					return
+				}
+			}
+		}
+	}()
+	return done
+}
+
+var ops int32
+
+type CopyWorker struct {
+	Overwrite bool
+	Merge bool
+}
+
+func (w *CopyWorker) handleJob(j CopyJob) ([]CopyJob, error)  {
+	var result []CopyJob
+
+	if j.name == "iterateCollection" {
+		return w.handleCollectionIterationJob(j)
+	}
+
+	if j.name == "iterateDocument" {
+		return w.handleDocumentIterationJob(j)
+	}
+
+	if j.name == "copyDocument" {
+		return w.handleCopyDocumentJob(j)
+	}
+
+	return result, nil
+}
+
+func (w *CopyWorker) handleCollectionIterationJob(j CopyJob) ([]CopyJob, error) {
+	var result []CopyJob
+
+	if next, err := j.collectionIterator.Next(); err != nil {
 		if err == iterator.Done {
-			return
+			return result, nil
 		}
+		return result, err
+	} else {
+		result = append(result,
+			j,
+			NewDocumentIterationJob(next, j.targetDocumentRef.Collection(next.ID)),
+		)
+		return result, nil
+	}
+}
+
+func (w *CopyWorker) handleDocumentIterationJob(j CopyJob) ([]CopyJob, error) {
+	var result []CopyJob
+
+	if next, err := j.documentRefIterator.Next(); err != nil {
+		if err == iterator.Done {
+			return result, nil
+		}
+		return result, err
+	} else {
+		result = append(result,
+			j,
+			NewCollectionIterationJob(next, j.targetCollectionRef.Doc(next.ID)),
+			NewDocumentCopyJob(next, j.targetCollectionRef.Doc(next.ID)),
+		)
+		return result, nil
+	}
+}
+
+func (w *CopyWorker) handleCopyDocumentJob(j CopyJob) ([]CopyJob, error) {
+	var result []CopyJob
+
+	targetDoc, err := j.targetDocumentRef.Get(context.Background())
+
+	if targetDoc == nil {
 		if err != nil {
-			log.Fatal(err)
-			return
+			log.Printf("get document %s error: %s\n", j.targetDocumentRef.Path, err)
+			return result, err
 		}
-		wg.Add(1)
-		go func(s, t *firestore.DocumentRef, wwg *sync.WaitGroup) {
-			defer wwg.Done()
-			copyDocument(s, t, option)
-		}(next, target.Doc(next.ID), &wg)
+		return result, nil
+	}
+
+	if targetDoc.Exists() && !w.Overwrite {
+		log.Printf("skipped document %s, because it already exists. use --overwrite to overwrite \n", j.targetDocumentRef.Path)
+		return result, nil
+	}
+
+	sourceDoc, err := j.documentRef.Get(context.Background())
+
+	if sourceDoc == nil {
+		if err != nil {
+			log.Printf("get document %s error: %s\n", j.documentRef.Path, err)
+			return result, err
+		}
+		return result, nil
+	}
+
+	var options []firestore.SetOption
+	if w.Merge {
+		options = append(options, firestore.MergeAll)
+	}
+
+	atomic.AddInt32(&ops, 1)
+
+	if sourceDoc.Exists() {
+		log.Printf("set document: %d, %s \n", ops, j.targetDocumentRef.Path)
+		_, err = j.targetDocumentRef.Set(context.Background(), sourceDoc.Data(), options...)
+		if err != nil {
+			log.Printf("copy document %s to %s error: %s\n", j.documentRef.Path, j.targetDocumentRef.Path, err)
+			return result, err
+		}
+	}
+
+	return result, nil
+}
+
+type CopyJob struct {
+	// iterateCollection iterateDocument copyDocument
+	name string
+
+	documentRef *firestore.DocumentRef
+
+	collectionIterator *firestore.CollectionIterator
+	targetDocumentRef *firestore.DocumentRef
+
+	documentRefIterator *firestore.DocumentRefIterator
+	targetCollectionRef *firestore.CollectionRef
+}
+
+func NewCollectionIterationJob(documentRef *firestore.DocumentRef, targetDocumentRef *firestore.DocumentRef) CopyJob {
+	return CopyJob{
+		name:                "iterateCollection",
+		collectionIterator:  documentRef.Collections(context.Background()),
+		targetDocumentRef:   targetDocumentRef,
+	}
+}
+
+
+func NewDocumentIterationJob(collectionRef *firestore.CollectionRef, targetCollectionRef *firestore.CollectionRef) CopyJob {
+	return CopyJob{
+		name:                "iterateDocument",
+		documentRefIterator: collectionRef.DocumentRefs(context.Background()),
+		targetCollectionRef: targetCollectionRef,
+	}
+}
+
+func NewDocumentCopyJob(documentRef *firestore.DocumentRef, targetDocumentRef *firestore.DocumentRef) CopyJob {
+	return CopyJob{
+		name:                "copyDocument",
+		documentRef:         documentRef,
+		targetDocumentRef:   targetDocumentRef,
 	}
 }
